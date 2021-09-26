616339e5f60482621bf30e197e95bd4fb621c1d9
Theo <bethuel47@gmail.com>
2021-09-26 17:16:46 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+const port = process.env.PORT || 3000;
+var express= require('express');
+const app = new express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+process.env.PWD = process.cwd()
+
+app.use(express.static(process.env.PWD + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/Login.html');
+});
+
+http.listen(port, () => {
+  console.log(``);
+});
+
+io.on('connection', function(socket) {
+    socket.on('event', function(data) {
+        io.emit('event', data);
+        console.log('event', data);
+    });
+});
+io.on('connection', function(socket) {
+    socket.on('eventJ', function(data) {
+        io.emit('eventJ', data);
+        console.log('eventJ', data);
+    });
+});
+io.on('connection', function(socket) {
+    socket.on('eventK', function(data) {
+        io.emit('eventK', data);
+        console.log('eventK', data);
+    });
+});
+io.on('connection', function(socket) {
+    socket.on('vida', function(data) {
+        console.log('vida', data);
+    });
+});
+io.on('connection', function(socket) {
+    socket.on('sanidade', function(data) {
+        console.log('sanidade', data);
+    });
+});
+io.on('connection', function(socket) {
+    socket.on('magia', function(data) {
+        console.log('magia', data);
+    });
+});
+io.on('connection', function(socket) {
+    socket.on('peso', function(data) {
+        console.log('peso', data);
+    });
+});
+io.sockets.on('connection', function(socket)
+ {
+socket.on('upcanvas', function(data){
+socket.broadcast.emit('get-data', data);
+});
+ });
